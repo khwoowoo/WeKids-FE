219c6c9b7d562b3321582e20cd74ac017141c798
jabin1221 <koreanjb1221@gmail.com>
2024-11-04 12:47:34 +0900

    [feat/#2]: 키패드 공통 컴포넌트 수정
    외부에서 number라는 변수를 통해 키패드의 값이 눌릴때마다 해당 값을
    읽어오도록 변경했습니다.

--- ui/components/KeyPad.js
+++ ui/components/KeyPad.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function KeyPad({
     buttonWidth = 'w-24',
     buttonHeight = 'h-12',
     fontFamily = 'Arial, sans-serif',
     fontSize = '1rem',
+    number = () => {}
 }) {
+    const [selected_number, setSelected_Number] = useState(null);
+
+    useEffect(() => {
+        number(selected_number);
+    }, [selected_number]);
+
+    const onClick = (e) => {
+        const value = e.currentTarget.innerText;
+        setSelected_Number(value);
+    };
+
     return (
         <div className="flex">
             <div className="grid grid-cols-3 bg-white">
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>1</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>2</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>3</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>4</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>5</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>6</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>7</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>8</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>9</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>00</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>0</button>
-                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`}>⌫</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>1</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>2</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>3</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>4</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>5</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>6</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>7</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>8</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>9</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>00</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>0</button>
+                <button className={`keypad-button ${buttonWidth} ${buttonHeight} text-center border`} onClick={onClick}>⌫</button>
             </div>
             <style jsx>{`
                 .keypad-button {

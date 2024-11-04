9e048fdddef80bf407932e1171ce1cefaf7a5e19
안찬웅 <1stevering@naver.com>
2024-11-04 15:24:04 +0900

    [fix/#3]: 사각형 tailwind 수정
    사각형 css tailwind로 변경

--- ui/components/Modal/Rectangle.js
+++ ui/components/Modal/Rectangle.js
@@ -2,17 +2,8 @@ import React from 'react';
 
 const Rectangle = () => {
     return (
-      <div
-        style={{
-          width: '400px',   
-          height: '60px',      
-          backgroundColor: '#FFFFFF',
-          borderRadius: '4px',
-          border: '1px solid #E0E0E0',
-          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-        }}
-      />
+      <div className="w-[400px] h-[60px] bg-white rounded-md border border-gray-300 shadow"></div>
     );
   };
-  
+
   export default Rectangle;

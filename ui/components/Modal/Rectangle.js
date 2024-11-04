6e9f32f421cb0edd486c7aeabcf090b25490fc5a
안찬웅 <1stevering@naver.com>
2024-11-04 11:41:34 +0900

    [feat/#3]: 사각형 구현
    사각형 구현했습니다.

---
+++ ui/components/Modal/Rectangle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rectangle = () => {
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '60px',
+          backgroundColor: '#FFFFFF',
+          borderRadius: '4px',
+        }}
+      />
+    );
+  };
+  
+  export default Rectangle;

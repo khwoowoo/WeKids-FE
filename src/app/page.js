84a3b9d2f1b760ef7a8bdd52df311c786065c095
yoon <cbj0010@naver.com>
2024-11-05 15:47:31 +0900

    [feat/#12]: root 수정하기

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,14 @@
-import Image from "next/image";
 import "./globals.css";
 import "../ui/Components/atoms/Upload";
 import Upload from "../ui/Components/atoms/Upload";
+import MainHome from "./[home]/page";
+import Header from "../ui/Components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="wooridaumL">Hello World적용됨????</h1>
-      <p className="wooridaumB">여기에만 적용하고 싶어요</p>
-      <Upload />
-      <MainHome />
-
-      <nav>네비게이션 바</nav>
+      <Header />
+      <MainHome className="flex justify-center items-center" />
     </div>
   );
 }

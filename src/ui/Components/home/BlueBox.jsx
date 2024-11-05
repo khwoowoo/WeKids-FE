e726566e0353ff076a27e7dd650f8fe106a3596f
yoon <cbj0010@naver.com>
2024-11-05 16:50:09 +0900

    [feat/#12]: 계좌 카드 뷰 위치 수정

--- src/ui/Components/home/BlueBox.jsx
+++ src/ui/Components/home/BlueBox.jsx
@@ -1,48 +1,55 @@
-// components/BlueBox.js
 import { Text } from "@radix-ui/themes";
 import Copy from "../atoms/CopyIcon";
 
 const BlueBox = () => {
-  // 예시 데이터
   const accountInfo = {
-    accountNumber: "123-456-7890",
-    balance: 3000000, // 잔액을 3,000,000으로 변경
+    accountNumber: "3333-0073-0030-03",
+    balance: 300000,
     name: "강현우",
   };
 
-  const transactions = [
-    { id: 1, date: "2024-11-01", description: "편의점 결제", amount: -5000 },
-    { id: 2, date: "2024-11-02", description: "월급", amount: 2000000 },
-    { id: 3, date: "2024-11-03", description: "커피", amount: -4500 },
-  ];
-
   return (
-    <div className="bg-mainColor w-[330px] h-[252px] rounded-[10px] flex flex-col justify-between">
-      <div className="text-white text-left mt-[35px] flex flex-col">
-        <div className="flex items-center space-x-2">
-          <Text className="wooridaumB">{accountInfo.accountNumber}</Text>
+    <div className="bg-mainColor w-[330px] h-[252px] text-white rounded-[10px] flex flex-col justify-between relative">
+      <div className="text-left px-5">
+        {/* 계좌번호 영역 */}
+        <div className="flex items-center space-x-2 mt-[25px]">
+          <Text className="wooridaumB text-sm">
+            {accountInfo.accountNumber}
+          </Text>
           <Copy />
         </div>
-        <Text className="wooridaumB mt-2 ">{accountInfo.name}</Text>
-        {/* 계좌 잔액 표시 */}
-        <Text className="wooridaumB mt-2">
-          {accountInfo.balance.toLocaleString()} 원
-        </Text>
+
+        {/* 이름 영역 */}
+        <Text className="wooridaumB text-xl mt-[32px]">{accountInfo.name}</Text>
       </div>
 
-      {/* 아래 "ㅜ" 모양 구분선 및 버튼 영역 */}
+      {/* 마스코트 이미지 */}
+      <div className="absolute right-4 bottom-16">
+        <img
+          src="/images/account_weebee.svg"
+          alt="Mascot"
+          className="w-16 h-16"
+        />
+      </div>
+
+      {/* 잔액 영역 - 마스코트 이미지 아래에 배치 */}
+      <Text className="wooridaumB text-[28px] mt-[48px] text-center">
+        {accountInfo.balance.toLocaleString()} 원
+      </Text>
+
+      {/* 하단 버튼 영역 */}
       <div className="mt-auto">
-        {/* 가로선 */}
-        <div className="flex items-center mt-4">
-          <div className="flex-1 border-t border-gray-300"></div>
-        </div>
+        {/* 구분선 */}
+        <div className="w-full h-[1px] bg-white/20"></div>
 
         {/* 버튼 영역 */}
-        <div className="flex mt-2">
-          <button className="flex-1 py-2 text-center border-r border-gray-300">
+        <div className="flex text-white">
+          <button className="flex-1 py-4 text-center border-r border-white/20 hover:bg-white/10 transition-colors">
             조회
           </button>
-          <button className="flex-1 py-2 text-center">이체</button>
+          <button className="flex-1 py-4 text-center hover:bg-white/10 transition-colors">
+            이체
+          </button>
         </div>
       </div>
     </div>

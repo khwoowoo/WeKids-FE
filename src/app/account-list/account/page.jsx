e95d601e337ae9656e694b3ce327fcd6b85c288f
jabin1221 <koreanjb1221@gmail.com>
2024-11-06 08:47:57 +0900

    [feat/#8]: 취소 버튼, ←버튼 라우터, select값에 따른 쿼리값 변경  구현
    두 버튼을 누르면 accout-list로 가게 했고, 상단뷰의 select값에 따라 쿼리
    id 값 변경

--- src/app/account-list/account/page.jsx
+++ src/app/account-list/account/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import KeyPad from '../../../ui/Components/atoms/KeyPad';
 import NextButton from '../../../ui/Components/atoms/Modal/NextButton';
 import Modal from '../../../ui/Components/atoms/Modal';
@@ -7,15 +8,15 @@ import Profile from '../../../ui/Components/atoms/Modal/Profile';
 import TransferButton from '../../../ui/Components/atoms/Modal/Transferbutton';
 
 const dummyData = [
-    { name: '구자빈', account: '111-111-111', balance: 1000000, bank: '우리은행' },
-    { name: '강현우', account: '222-222-222', balance: 2000000, bank: '우리은행' },
-    { name: '안찬웅', account: '333-333-333', balance: 3000000, bank: '우리은행' },
-    { name: '조예은', account: '444-444-444', balance: 4000000, bank: '우리은행' },
-    { name: '최윤정', account: '555-555-555', balance: 5000000, bank: '우리은행' },
+    { id: 1, name: '구자빈', account: '111-111-111', balance: 1000000, bank: '우리은행' },
+    { id: 2, name: '강현우', account: '222-222-222', balance: 2000000, bank: '우리은행' },
+    { id: 3, name: '안찬웅', account: '333-333-333', balance: 3000000, bank: '우리은행' },
+    { id: 4, name: '조예은', account: '444-444-444', balance: 4000000, bank: '우리은행' },
+    { id: 5, name: '최윤정', account: '555-555-555', balance: 5000000, bank: '우리은행' },
 ];
 
 const receive_dummyData = [
-    { name: '김우리', account: '666-666-666'},
+    { name: '김우리', account: '666-666-666', bank: '우리은행'},
 ];
 
 export default function Page() {
@@ -24,6 +25,7 @@ export default function Page() {
     const [selectedUser, setSelectedUser] = useState(dummyData[0]); // TODO : 추후에 현재 유저 데이터 생길시 더미데이터 빼고 외부값 넣기
     const [receiveUser, setReceiveUser] = useState(receive_dummyData[0]); // TODO : 추후에 받는 사람 데이터 생길시 더미데이터 빼고 외부값 넣기
     const [isShaking, setIsShaking] = useState(false);
+    const router = useRouter();
 
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
@@ -35,7 +37,11 @@ export default function Page() {
           }, 500);
           return () => clearTimeout(timeout);
         }
-      }, [isShaking]);
+    }, [isShaking]);
+
+    useEffect(() => {
+        router.push(`/account-list/account?id=${selectedUser.id}`);
+    }, [selectedUser])
 
     const handleNumberClick = (num) => {
         if(num == '⌫'){
@@ -63,6 +69,16 @@ export default function Page() {
         setAmount(selectedUser.balance);
     };
 
+    const routeControll = (text) => {
+        if(text == 'cancel'){
+            router.push('/account-list');
+        }
+        else{
+            router.push('/account-list/account/done');
+        }
+        
+    }
+
     const handleAddAmount = (increment) => {
         if(amount + increment > selectedUser.balance){
             setAmount(selectedUser.balance);
@@ -78,12 +94,13 @@ export default function Page() {
         const selectedName = e.target.value;
         const user = dummyData.find((user) => user.name === selectedName);
         setSelectedUser(user);
+       
     };
   
     return (
     <div className="flex flex-col h-screen overflow-hidden bg-gray-100">
         <div className="flex justify-between items-center w-full p-4 top-0 absolute">
-            <button className="text-lg">←</button>
+            <button className="text-lg" onClick={() => routeControll("cancel")}>←</button>
             <div className="text-center">
                 <div>
                     <select
@@ -100,14 +117,15 @@ export default function Page() {
                 </div>
                 <div className="text-xs text-gray-500"> {selectedUser.bank + " " +  selectedUser.account}</div>
             </div>
-            <button className="text-lg">취소</button>
+            <button className="text-lg" onClick={() => routeControll("cancel")}>취소</button>
         </div>
         <div className="flex flex-col items-center flex-grow">
             <div className="flex flex-col text-center justify-center h-1/2 ">
                 <div className={`${isShaking ? 'shake-animation': ''} text-4xl font-bold ${isShaking ? 'text-red-600' : 'text-slate-900'}`}>{amount.toLocaleString()}원</div>
                 <div className="text-red-600">{isShaking ? `${selectedUser.balance.toLocaleString()}원 까지만 이체 가능합니다.` : ''}</div>
             </div>
-            <div className="flex space-x-4 ">
+            <div className="bottom-0 fixed">
+            <div className="flex space-x-4 justify-center">
                 <button className="px-4 py-1 bg-gray-200 rounded" onClick={() => handleAddAmount(10000)}>
                 +1만
                 </button>
@@ -121,7 +139,7 @@ export default function Page() {
                 전액
                 </button>
             </div>
-            <div className="bottom-0 fixed">
+            
                 <div className="w-[393px] mt-9">
                     <KeyPad number={handleNumberClick} buttonHeight='h-14' buttonWidth='w-[393px]'/>
                 </div>
@@ -131,7 +149,7 @@ export default function Page() {
                 isOpen={isModalOpen}
                 onClose={closeModal}
                 width="393px"
-                height="45vh"
+                height="47vh"
                 translateY="0%"
                 delete_button={true}
             >
@@ -139,11 +157,11 @@ export default function Page() {
                     <Profile />
                     <p className="text-base mt-2"><span className="font-bold text-lg">{receiveUser.name}</span>님에게  <span className="font-bold text-lg">{amount.toLocaleString()}원</span> </p>
                     <p className="text-base mt-1">이체하시겠습니까?</p>
-                    <p className="text-xs mt-5 text-gray-400">받는계좌 :  {selectedUser.bank + " " + selectedUser.account}</p>
+                    <p className="text-xs mt-5 text-gray-400">받는계좌 :  {receiveUser.bank + " " + receiveUser.account}</p>
                 </div>
-                <div className="flex space-x-3 mt-9">
+                <div className="flex space-x-3 mt-7">   
                     <TransferButton text={"취소"} onClick={closeModal}/>
-                    <NextButton text={"이체하기"} />
+                    <NextButton text={"이체하기"} onClick={() => routeControll("done")} />
                 </div>
             </Modal>
         </div>

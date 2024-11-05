7cef589e7b30f58f4ee7a6314266b264f3ad57f5
jabin1221 <koreanjb1221@gmail.com>
2024-11-06 00:33:31 +0900

    [feat/#7]: 라우팅 처리
    박스를 누를 경우 해당 id값을 쿼리에 담아 account-list/account로 전달

--- src/app/account-list/page.jsx
+++ src/app/account-list/page.jsx
@@ -1,45 +1,48 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import TransactionList from "@/src/ui/Components/atoms/TransactionList";
 
 const dummyData = [
-  { id: 1, name: '안찬웅', bank: '우리은행', account: '1002-21-123456' },
-  { id: 2, name: '구자빈', bank: '우리은행', account: '1002-21-123456' },
-  { id: 3, name: '최윤정', bank: '우리은행', account: '1002-21-123456' },
-  { id: 4, name: '조예은', bank: '우리은행', account: '1002-21-123456' },
+    { id: 1, name: '안찬웅', bank: '우리은행', account: '1002-21-123456' },
+    { id: 2, name: '구자빈', bank: '우리은행', account: '1002-21-123456' },
+    { id: 3, name: '최윤정', bank: '우리은행', account: '1002-21-123456' },
+    { id: 4, name: '조예은', bank: '우리은행', account: '1002-21-123456' },
 ];
 
 export default function Page() {
-  const [selectedId, setSelectedId] = useState(null);
+    const [selectedId, setSelectedId] = useState(null);
+    const router = useRouter();
   
+    useEffect(() => {
+        if (selectedId !== null) {
+            router.push(`/account-list/account?id=${selectedId}`);
+        }
+    }, [selectedId]);
 
-  const handleSelect = (id) => {
-    setSelectedId(id);
-    
-  };
+    const handleSelect = (id) => {
+        setSelectedId(id);   
+    };
 
-  return (
+    return (
     <div className="max-w-md mx-auto bg-white shadow-lg h-screen">
-      <div className="flex justify-between p-4">
-        <h1 className="text-lg font-bold">이체</h1>
-        <button className="text-gray-500">닫기</button>
-      </div>
-      
-      <div>
-        {dummyData.map((user) => (
-          <TransactionList // 추후에 사진을 넣어야함
-            key={user.id}
-            name={user.name}
-            account={user.account}
-            bank={user.bank}
-            isSelected={user.id === selectedId}
-            onClick={() => handleSelect(user.id)}
-          />
-        ))}
-      </div>
-        
-      
+        <div className="flex justify-between p-4">
+            <h1 className="text-lg font-bold">이체</h1>
+            <button className="text-gray-500">닫기</button>
+        </div>
+        <div>
+            {dummyData.map((user) => (
+            <TransactionList // 추후에 사진을 넣어야함
+                key={user.id}
+                name={user.name}
+                account={user.account}
+                bank={user.bank}
+                isSelected={user.id === selectedId}
+                onClick={() => handleSelect(user.id)}
+            />
+            ))}
+        </div>  
     </div>
   );
 }

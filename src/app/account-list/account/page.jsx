81709ad196fb8cd096362ca45efd670c03aa59f3
jabin1221 <koreanjb1221@gmail.com>
2024-11-05 16:20:23 +0900

    [feat/#8]: 모달창 구현 및 모달 내의 컴포넌트 구현
    모달창과 모달창 내부의 컴포넌트를 구현했습니다.

--- src/app/account-list/account/page.jsx
+++ src/app/account-list/account/page.jsx
@@ -2,6 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import KeyPad from '../../../ui/Components/atoms/KeyPad';
 import NextButton from '../../../ui/Components/atoms/Modal/NextButton';
+import Modal from '../../../ui/Components/atoms/Modal';
+import Profile from '../../../ui/Components/atoms/Modal/Profile';
+import TransferButton from '../../../ui/Components/atoms/Modal/Transferbutton';
 
 const dummyData = [
     { name: '구자빈', account: '111-111-111', balance: 1000000, bank: '우리은행' },
@@ -11,9 +14,18 @@ const dummyData = [
     { name: '최윤정', account: '555-555-555', balance: 5000000, bank: '우리은행' },
   ];
 
+const receive_dummyData = [
+    { name: '김우리', account: '111-111-111'},
+  ];
+
 export default function Page() {
     const [amount, setAmount] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedUser, setSelectedUser] = useState(dummyData[0]);
+    const [receiveUser, setReceiveUser] = useState(receive_dummyData[0]); // TODO : 추후에 받는 사람 데이터 생길시 더미데이터 빼고 외부값 넣기
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     const handleNumberClick = (num) => {
         if(num == '⌫'){
@@ -95,8 +107,27 @@ export default function Page() {
             <div className="w-[393px] mt-9">
                 <KeyPad number={handleNumberClick} buttonHeight='h-14' buttonWidth='w-[393px]'/>
             </div>
-                <NextButton width='w-[393px]' radius = ''/>
+                <NextButton onClick={openModal} width='w-[393px]' radius = ''/>
             </div>
+            <Modal
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                width="393px"
+                height="45vh"
+                translateY="0%"
+                delete_button={false}
+            >
+            <div className="flex flex-col items-center">
+                <Profile />
+                <p className="text-base mt-2"><span className="font-bold text-lg">{receiveUser.name}</span>님에게  <span className="font-bold text-lg">{amount.toLocaleString()}원</span> </p>
+                <p className="text-base mt-1">이체하시겠습니까?</p>
+                <p className="text-xs mt-5 text-gray-400">받는계좌 :  {selectedUser.bank + " " + selectedUser.account}</p>
+            </div>
+                <div className="flex space-x-3 mt-9">
+                    <TransferButton text={"취소"} onClick={closeModal}/>
+                    <NextButton text={"이체하기"} />
+                </div>
+            </Modal>
         </div>
     </div>
   );

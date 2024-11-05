2da27dc9a427e394e182ac67e79ecae4b1a41fb0
jabin1221 <koreanjb1221@gmail.com>
2024-11-06 00:16:54 +0900

    [feat/#7]: 이체, 닫기 버튼 구현
    이체, 닫기 버튼 상단에 구현

---
+++ src/app/account-list/page.jsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function Page() {
+
+
+  return (
+    <div className="max-w-md mx-auto bg-white shadow-lg h-screen">
+      <div className="flex justify-between p-4">
+        <h1 className="text-lg font-bold">이체</h1>
+        <button className="text-gray-500">닫기</button>
+      </div>
+    </div>
+  );
+}

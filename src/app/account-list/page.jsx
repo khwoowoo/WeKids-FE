e3928a00129813e713bb7fd200307d3b637ce30e
jabin1221 <koreanjb1221@gmail.com>
2024-11-06 00:29:16 +0900

    [feat/#7]: 이체 리스트 전체 뷰 구현
    이체 리스트 전체 뷰를 구현했습니다. 배경색이 흰색이라 잘 구분이 되지
    않는 것은 고민해보겠습니다.

--- src/app/account-list/page.jsx
+++ src/app/account-list/page.jsx
@@ -1,7 +1,23 @@
 'use client';
 
-export default function Page() {
+import React, { useState, useEffect } from 'react';
+import TransactionList from "@/src/ui/Components/atoms/TransactionList";
 
+const dummyData = [
+  { id: 1, name: '안찬웅', bank: '우리은행', account: '1002-21-123456' },
+  { id: 2, name: '구자빈', bank: '우리은행', account: '1002-21-123456' },
+  { id: 3, name: '최윤정', bank: '우리은행', account: '1002-21-123456' },
+  { id: 4, name: '조예은', bank: '우리은행', account: '1002-21-123456' },
+];
+
+export default function Page() {
+  const [selectedId, setSelectedId] = useState(null);
+  
+
+  const handleSelect = (id) => {
+    setSelectedId(id);
+    
+  };
 
   return (
     <div className="max-w-md mx-auto bg-white shadow-lg h-screen">
@@ -9,6 +25,21 @@ export default function Page() {
         <h1 className="text-lg font-bold">이체</h1>
         <button className="text-gray-500">닫기</button>
       </div>
+      
+      <div>
+        {dummyData.map((user) => (
+          <TransactionList // 추후에 사진을 넣어야함
+            key={user.id}
+            name={user.name}
+            account={user.account}
+            bank={user.bank}
+            isSelected={user.id === selectedId}
+            onClick={() => handleSelect(user.id)}
+          />
+        ))}
+      </div>
+        
+      
     </div>
   );
 }

efecd12921fb1deda276bafa09bbc9a761cd7d3f
yoon <cbj0010@naver.com>
2024-11-06 11:27:38 +0900

    [feat/#12]: header 생성
    header에 들어가는 아이콘이랑 로고 만들기

---
+++ src/ui/layout/Header.js
@@ -0,0 +1,18 @@
+// components/Header.js
+
+import AlarmIcon from "@/public/icons/AlarmIcon";
+import PersonIcon from "@/public/icons/personIcon";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center px-5 py-3 bg-gray-100 border-b border-gray-300">
+      <img src="/images/logo.svg" alt="Logo" className="h-6 w-auto" />
+      <div className="flex space-x-4">
+        {/* 알람 아이콘 */}
+        <AlarmIcon className="h-6 w-6 cursor-pointer" />
+        {/* 프로필 아이콘 */}
+        <PersonIcon className="h-6 w-6 cursor-pointer" />
+      </div>
+    </header>
+  );
+}

171f660e79db0a7cdcdeaf33210b238caf6052b4
jabin1221 <koreanjb1221@gmail.com>
2024-11-06 12:42:28 +0900

    [feat/#7]: zustand, 스크롤바 숨김 처리
    zustand로 상태변수 관리 및 스크롤바 숨김 기능을 추가했습니다.

--- src/app/account-list/page.jsx
+++ src/app/account-list/page.jsx
@@ -3,46 +3,66 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import TransactionList from "@/src/ui/Components/atoms/TransactionList";
+import useTransactionStore from "../../stores/useTransactionStore"
 
 const dummyData = [
-    { id: 1, name: '안찬웅', bank: '우리은행', account: '1002-21-123456' },
-    { id: 2, name: '구자빈', bank: '우리은행', account: '1002-21-123456' },
-    { id: 3, name: '최윤정', bank: '우리은행', account: '1002-21-123456' },
-    { id: 4, name: '조예은', bank: '우리은행', account: '1002-21-123456' },
+    { id: 1, name: '구자빈', account: '111-111-111' },
+    { id: 2, name: '강현우', account: '222-222-222' },
+    { id: 3, name: '안찬웅', account: '333-333-333' },
+    { id: 4, name: '조예은', account: '444-444-444' },
+    { id: 5, name: '최윤정', account: '555-555-555' },
+    { id: 6, name: '김우리', account: '666-666-666' },
+    { id: 7, name: '가우리', account: '777-777-777' },
+    { id: 8, name: '나우리', account: '888-888-888' },
+    { id: 9, name: '다우리', account: '999-999-999' },
+    { id: 10, name: '라우리', account: '000-000-000' },
 ];
 
 export default function Page() {
-    const [selectedId, setSelectedId] = useState(null);
-    const router = useRouter();
+  const [selectedId, setSelectedId] = useState(null);
+  const router = useRouter();
+  const setSelectedAccount = useTransactionStore((state) => state.setSelectedAccount);
   
-    useEffect(() => {
-        if (selectedId !== null) {
-            router.push(`/account-list/account?id=${selectedId}`);
+  useEffect(() => {
+    if (selectedId !== null) {
+        const user = dummyData.find((user) => user.id === selectedId);
+        if (user) {
+            setSelectedAccount({
+                id: user.id,
+                name: user.name,
+                account: user.account
+            });
+            router.push(`/account-list/account`);
         }
-    }, [selectedId]);
+      }
+  }, [selectedId, setSelectedAccount, router]);
 
-    const handleSelect = (id) => {
-        setSelectedId(id);   
-    };
+  const handleSelect = (id) => {
+    setSelectedId(id);
+    
+  };
 
-    return (
-    <div className="max-w-md mx-auto bg-white shadow-lg h-screen">
-        <div className="flex justify-between p-4">
-            <h1 className="text-lg font-bold">이체</h1>
-            <button className="text-gray-500">닫기</button>
-        </div>
-        <div>
-            {dummyData.map((user) => (
-            <TransactionList // 추후에 사진을 넣어야함
-                key={user.id}
-                name={user.name}
-                account={user.account}
-                bank={user.bank}
-                isSelected={user.id === selectedId}
-                onClick={() => handleSelect(user.id)}
-            />
-            ))}
-        </div>  
+  return (
+    <div className="max-w-md mx-auto bg-gray-100 shadow-lg h-screen flex flex-col">
+      <div className="flex justify-between p-4">
+        <h1 className="text-lg font-bold">이체</h1>
+        <button className="text-gray-500">닫기</button>
+      </div>
+      
+      <div className="flex-1 overflow-y-auto scrollbar-hide">
+        {dummyData.map((user) => (
+          <TransactionList // 추후에 사진을 넣어야함
+            key={user.id}
+            name={user.name}
+            account={user.account}
+            bank={"우리은행"}
+            isSelected={user.id === selectedId}
+            onClick={() => handleSelect(user.id)}
+          />
+        ))}
+      </div>
+        
+      
     </div>
   );
 }

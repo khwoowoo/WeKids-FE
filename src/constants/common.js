ccf5bc6a037881a46eb446c5667616ec46530052
jabin1221 <koreanjb1221@gmail.com>
2024-11-07 15:20:00 +0900

    [refact/#24]: 변경사항 종합

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,35 +1,39 @@
-// src/utils/characterInfo.js
+// src/constants/characterInfo.js
 
 export const characterInfoMap = {
   WEBEE: {
     name: "위비",
-    imagePath: "/images/account_weebee.svg", // 위비 이미지 경로
+    imagePath: "/images/accountWeebeeImg.svg", // 위비 이미지 경로
     colorClass: "mainColor", // CSS 클래스 이름으로 색상 관리
   },
   BOMBOM: {
     name: "봄봄",
-    imagePath: "/images/bombom.svg", // 봄봄 이미지 경로
+    imagePath: "/images/bombomImg.svg", // 봄봄 이미지 경로
     colorClass: "colorBombom", // CSS 클래스 이름으로 색상 관리
   },
   DALBO: {
     name: "달보",
-    imagePath: "/images/dalbo.svg", // 달보 이미지 경로
+    imagePath: "/images/dalboImg.svg", // 달보 이미지 경로
     colorClass: "colorDalbo", // CSS 클래스 이름으로 색상 관리
   },
   DUJI: {
     name: "두지",
-    imagePath: "/images/dalbo.svg", // 두지 이미지 경로
+    imagePath: "/images/dalboImg.svg", // 두지 이미지 경로
     colorClass: "colorDuji", // CSS 클래스 이름으로 색상 관리
   },
   KU: {
     name: "쿠",
-    imagePath: "/images/dalbo.svg", // 쿠 이미지 경로
+    imagePath: "/images/dalboImg.svg", // 쿠 이미지 경로
     colorClass: "colorKu", // CSS 클래스 이름으로 색상 관리
   },
   PUDDING: {
     name: "푸딩",
-    imagePath: "/images/dalbo.svg", // 푸딩 이미지 경로
+    imagePath: "/images/dalboImg.svg", // 푸딩 이미지 경로
     colorClass: "colorPudding", // CSS 클래스 이름으로 색상 관리
   },
   //Todo 두지,쿠,푸딩에 맞는 이미지로 경로 수정해줘야 함
+};
+
+export const urlPath = {
+
 };
